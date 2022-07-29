9717dd65974dbbe4e4a4242296248c52e8b30f6c
Hugo Virgen <virgenherrera@gmail.com>
2022-07-29 16:40:47 -0500

    * rewrite download diel links.

--- src/app/core/components/shared-folder/shared-folder.component.ts
+++ src/app/core/components/shared-folder/shared-folder.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FolderInfo } from '../../../api/models/folder-info.model';
 import { SharedFolderService } from '../../../api/services/shared-folder.service';
-import { Endpoint } from '../../enums/endpoint.enum';
 
 @Component({
   selector: 'app-shared-folder',
@@ -18,29 +17,10 @@ export class SharedFolderComponent implements OnInit {
     const folderInfo$ = this.sharedFolderService.getFolderInfo().subscribe({
       next: (value) => {
         this.folderInfo = value;
-
-        // this.folderInfo.files = Array(7).fill(this.folderInfo.files[0]);
-
-        this.folderInfo.files.forEach((file) =>
-          this.getDownloadUrl(file.fileName),
-        );
       },
-      complete() {
+      complete: () => {
         folderInfo$.unsubscribe();
       },
     });
   }
-
-  getDownloadUrl(fileName: string) {
-    const endpoint = pathJoin('/api/v1/', Endpoint.file, this.path, fileName);
-
-    console.log({ endpoint });
-  }
-}
-
-function pathJoin(...paths: string[]): string {
-  const separator = '/';
-  const replace = new RegExp(separator + '{1,}', 'g');
-
-  return paths.join(separator).replace(replace, separator);
 }
