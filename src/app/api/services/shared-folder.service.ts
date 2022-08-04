00a955870dfeb3ab7aea852db4f6dc9909e2cb1b
Hugo Virgen <virgenherrera@gmail.com>
2022-08-03 20:21:17 -0500

    Revert "feat: refresh folder path content after upload (#7)" (#8)
    This reverts commit 6a3bca25b842c445134a186514f419a7098934d7.

--- src/app/api/services/shared-folder.service.ts
+++ src/app/api/services/shared-folder.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Endpoint } from '../../core/enums/endpoint.enum';
 import { FolderInfo } from '../models/folder-info.model';
 
@@ -9,19 +9,12 @@ import { FolderInfo } from '../models/folder-info.model';
 })
 export class SharedFolderService {
   private endpoint = `/api/v1/${Endpoint.sharedFolder}`;
-  readonly FolderInfoSubject = new BehaviorSubject<FolderInfo>(null);
 
   constructor(private http: HttpClient) {}
 
   getFolderInfo(path = ''): Observable<FolderInfo> {
-    this.FolderInfoSubject.next(null);
-
     const url = `${this.endpoint}${path}`;
 
-    return this.http.get(url).pipe(
-      map((res: any) => new FolderInfo(res)),
-      tap((info) => this.FolderInfoSubject.next(info)),
-      map(() => this.FolderInfoSubject.value),
-    );
+    return this.http.get(url).pipe(map((res: any) => new FolderInfo(res)));
   }
 }
