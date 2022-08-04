f217ab8c15580b72c975acb606a39552ec7ca0e9
Hugo Virgen <virgenherrera@gmail.com>
2022-08-03 20:24:06 -0500

    Feat/refresh path after upload (#9)
    * feat: refresh folder path content after upload
    
    - refector SharedFolderService to store response as subject.
    - implement subject sub in SharedFolderComponent.

--- src/app/api/services/shared-folder.service.ts
+++ src/app/api/services/shared-folder.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { Endpoint } from '../../core/enums/endpoint.enum';
 import { FolderInfo } from '../models/folder-info.model';
 
@@ -9,12 +9,19 @@ import { FolderInfo } from '../models/folder-info.model';
 })
 export class SharedFolderService {
   private endpoint = `/api/v1/${Endpoint.sharedFolder}`;
+  readonly folderInfoSubject = new BehaviorSubject<FolderInfo>(null);
 
   constructor(private http: HttpClient) {}
 
   getFolderInfo(path = ''): Observable<FolderInfo> {
+    this.folderInfoSubject.next(null);
+
     const url = `${this.endpoint}${path}`;
 
-    return this.http.get(url).pipe(map((res: any) => new FolderInfo(res)));
+    return this.http.get(url).pipe(
+      map((res: any) => new FolderInfo(res)),
+      tap((info) => this.folderInfoSubject.next(info)),
+      map(() => this.folderInfoSubject.value),
+    );
   }
 }
